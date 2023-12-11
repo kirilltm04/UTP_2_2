951a6fd97c9c1b4964e5fe39b6fb420d1aa7af98
kirilltm04 <92577092+kirilltm04@users.noreply.github.com>
2023-12-11 19:15:49 +0100

    comm

--- src/Main.java
+++ src/Main.java
@@ -13,21 +13,21 @@ public class Main {
         pl.getLangsMap().forEach((k,v)->System.out.println(k+ " = " + v));
         System.out.println("@2 Programmers Map:");
         pl.getProgsMap().forEach((k,v)->System.out.println(k+ " = " + v));
-//        System.out.println("@3 Languages sorted by the number of programmers:");
-//        pl.getLangsMapSortedByNumOfProgs()
-//                .forEach((k,v)->System.out.println(k+ " = " + v));
-//        System.out.println("@4 Programmers sorted by number of languages:");
-//        pl.getProgsMapSortedByNumOfLangs()
-//                .forEach((k,v)->System.out.println(k+ " = " + v));
-//        System.out.println("@5 Original language map unchanged:");
-//        pl.getLangsMap().forEach((k,v)->System.out.println(k+ " = " + v));
-//        System.out.println("@6 Original programmers map unchanged:");
-//        pl.getProgsMap().forEach((k,v)->System.out.println(k+ " = " + v));
-//        System.out.println("@7 Map of programmers knowing more than 1 language:");
-//        pl.getProgsMapForNumOfLangsGreaterThan(1)
-//                .forEach((k,v)->System.out.println(k+ " = " + v));
-//        System.out.println("@8 The original developer map not altered:");
-//        pl.getProgsMap().forEach((k,v)->System.out.println(k+ " = " + v));
+        System.out.println("@3 Languages sorted by the number of programmers:");
+        pl.getLangsMapSortedByNumOfProgs()
+                .forEach((k,v)->System.out.println(k+ " = " + v));
+        System.out.println("@4 Programmers sorted by number of languages:");
+        pl.getProgsMapSortedByNumOfLangs()
+                .forEach((k,v)->System.out.println(k+ " = " + v));
+        System.out.println("@5 Original language map unchanged:");
+        pl.getLangsMap().forEach((k,v)->System.out.println(k+ " = " + v));
+        System.out.println("@6 Original programmers map unchanged:");
+        pl.getProgsMap().forEach((k,v)->System.out.println(k+ " = " + v));
+        System.out.println("@7 Map of programmers knowing more than 1 language:");
+        pl.getProgsMapForNumOfLangsGreaterThan(1)
+                .forEach((k,v)->System.out.println(k+ " = " + v));
+        System.out.println("@8 The original developer map not altered:");
+        pl.getProgsMap().forEach((k,v)->System.out.println(k+ " = " + v));
     }
 
 }
--- src/ProgLang.java
+++ src/ProgLang.java
@@ -2,25 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class ProgLang {
-    public HashMap<String, List<String>> map;
+    public LinkedHashMap<String, List<String>> map;
 
-    public ProgLang(String path) throws FileNotFoundException {
+    public LinkedHashMap<String, List<String>> reader(String path) throws FileNotFoundException {
         File file = new File(path);
         Scanner sc = new Scanner(file);
-        this.map = new HashMap<>();
+        LinkedHashMap<String, List<String>> ans = new LinkedHashMap<>();
         while (sc.hasNextLine()) {
             String l = sc.nextLine();
             List<String> arr = List.of(l.split("\t"));
-            this.map.put(arr.get(0), arr.subList(1, arr.size()));
+            ans.put(arr.get(0), arr.subList(1, arr.size()));
         }
+        return ans;
     }
 
-    public HashMap<String, List<String>> getLangsMap() {
+    public ProgLang(String path) throws FileNotFoundException {
+        this.map = reader(path);
+    }
+
+    public LinkedHashMap<String, List<String>> getLangsMap() {
         return this.map;
     }
 
-    public HashMap<String, List<String>> getProgsMap() {
-        HashMap<String, List<String>> ans = new LinkedHashMap<>();
+    public void reversed(LinkedHashMap<String, List<String>> m) {
+        ArrayList<String> keys = new ArrayList<>(m.keySet());
+        ArrayList<List<String>> values = new ArrayList<>(m.values());
+        Collections.reverse(keys);
+        Collections.reverse(values);
+        m.clear();
+        for (int i = 0; i < keys.size(); i++) {
+            m.put(keys.get(i), values.get(i));
+        }
+    }
+
+    public LinkedHashMap<String, List<String>> getProgsMap() {
+        LinkedHashMap<String, List<String>> ans = new LinkedHashMap<>();
         for (Map.Entry<String, List<String>> m : this.map.entrySet()) {
             String str = m.getKey();
             List<String> lst = m.getValue();
@@ -31,4 +47,31 @@ public class ProgLang {
         return ans;
     }
 
+    public LinkedHashMap<String, List<String>> getLangsMapSortedByNumOfProgs() {
+        LinkedHashMap<String, List<String>> sorted = new LinkedHashMap<>();
+        this.map.entrySet().stream()
+                .sorted(Comparator.comparingInt(entry -> entry.getValue().size()))
+                .forEach(entry -> sorted.put(entry.getKey(), entry.getValue()));
+        reversed(sorted);
+        return sorted;
+    }
+
+    public LinkedHashMap<String, List<String>> getProgsMapSortedByNumOfLangs() {
+        LinkedHashMap<String, List<String>> progs = getProgsMap();
+        LinkedHashMap<String, List<String>> sortedMap = new LinkedHashMap<>();
+
+        progs.entrySet().stream()
+                .sorted(Comparator.comparingInt(entry -> entry.getValue().size()))
+                .forEach(entry -> sortedMap.put(entry.getKey(), entry.getValue()));
+        reversed(sortedMap);
+        return sortedMap;
+    }
+
+    public LinkedHashMap<String, List<String>> getProgsMapForNumOfLangsGreaterThan(int num) {
+        LinkedHashMap<String, List<String>> progs = getProgsMap();
+        return progs.entrySet().stream()
+                .filter(entry -> entry.getValue().size() > num)
+                .collect(LinkedHashMap::new, (map, entry) -> map.put(entry.getKey(), entry.getValue()), Map::putAll);
+    }
 }
+
